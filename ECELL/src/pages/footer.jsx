803207e064b9bb5214e2d450e4fe6f3c3bd3c473
rupanjanas <saharupanjana04@gmail.com>
2025-06-20 10:34:48 +0530

    update

--- ECELL/src/pages/footer.jsx
+++ ECELL/src/pages/footer.jsx
@@ -97,20 +97,20 @@ function Footer() {
             </svg>
           </a>
           {/* Instagram */}
-          <a href="#" className="flex items-center justify-center w-8 h-8 rounded-md border border-blue-600 bg-gray-700 hover:bg-blue-600 transition-colors duration-200">
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 text-white" fill="currentColor" viewBox="0 0 24 24">
+          <a href="#" className="flex items-center justify-center w-8 h-8 ">
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8 text-white" fill="currentColor" viewBox="0 0 24 24">
               <path d="M12 2.163c3.204 0 3.584.012 4.85.07c3.252.148 4.772 1.668 4.92 4.92.058 1.265.07 1.645.07 4.85 0 3.204-.012 3.584-.07 4.85-1.92 3.252-1.668 4.772-4.92 4.92-1.265.058-1.645.07-4.85.07-3.204 0-3.584-.012-4.85-.07-3.252-1.92-4.772-1.668-4.92-4.92-.058-1.265-.07-1.645-.07-4.85 0-3.204.012-3.584.07-4.85 1.92-3.252 1.668-4.772 4.92-4.92 1.265-.058 1.645-.07 4.85-.07zm0-2.163c-3.259 0-3.667.014-4.947.072-4.358.2-6.78 2.623-6.98 6.98-.059 1.28-.073 1.689-.073 4.948 0 3.259.014 3.668.072 4.948.2 4.358 2.623 6.78 6.98 6.98 1.28.058 1.689.073 4.948.073 3.259 0 3.668-.014 4.948-.072 4.358-.2 6.78-2.623 6.98-6.98.058-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.2-4.358-2.623-6.78-6.98-6.98-1.28-.059-1.689-.073-4.948-.073zm0 5.838c-3.403 0-6.162 2.759-6.162 6.162s2.759 6.162 6.162 6.162 6.162-2.759 6.162-6.162c0-3.403-2.759-6.162-6.162-6.162zm0 10.162c-2.209 0-4-1.791-4-4s1.791-4 4-4 4 1.791 4 4-1.791 4-4 4zm6.406-11.845c-.796 0-1.441.645-1.441 1.44s.645 1.44 1.441 1.44c.795 0 1.439-.645 1.439-1.44s-.644-1.44-1.439-1.44z"/>
             </svg>
           </a>
           {/* YouTube */}
-          <a href="#" className="flex items-center justify-center w-8 h-8 rounded-md border border-blue-600 bg-gray-700 hover:bg-blue-600 transition-colors duration-200">
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 text-white" fill="currentColor" viewBox="0 0 24 24">
+          <a href="#" className="flex items-center justify-center w-8 h-8 ">
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8 text-white" fill="currentColor" viewBox="0 0 24 24">
               <path d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm6.262 16.281c-.183.671-.62 1.258-1.205 1.716-.606.471-1.309.774-2.115.895-1.503.228-3.328.322-5.023.322-1.696 0-3.52-.094-5.023-.322-.806-.121-1.509-.424-2.115-.895-.585-.458-1.022-1.045-1.205-1.716-.183-.671-.277-1.564-.277-2.481 0-.917.094-1.81.277-2.481.183-.671.62-1.258 1.205-1.716.606-.471 1.309-.774 2.115-.895 1.503-.228 3.328-.322 5.023-.322 1.696 0 3.52.094 5.023.322.806.121 1.509.424 2.115.895.585.458 1.022 1.045 1.205 1.716.183.671.277 1.564.277 2.481 0 .917-.094 1.81-.277 2.481zm-7.262-6.281l4.998 2.5-4.998 2.5v-5z"/>
             </svg>
           </a>
           {/* Facebook */}
-          <a href="#" className="flex items-center justify-center w-8 h-8 rounded-md border border-blue-600 bg-gray-700 hover:bg-blue-600 transition-colors duration-200">
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 text-white" fill="currentColor" viewBox="0 0 24 24">
+          <a href="#" className="flex items-center justify-center w-8 h-8">
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8 text-white" fill="currentColor" viewBox="0 0 24 24">
               <path d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm3 8h-1.353c-.567 0-.713.298-.713.847v1.653h2.353l-.261 2.353h-2.092v7.147h-3v-7.147h-2v-2.353h2v-1.908c0-1.092.516-2.739 2.771-2.739h2.229v3z"/>
             </svg>
           </a>
